bde7b239a3f22e00fc4db0407f94741f742b3943
Ricool06 <Ricool06@users.noreply.github.com>
2020-07-25 00:09:28 +0100

    feat: write function to load model

---
+++ projects/func/src/func/model_loader.py
@@ -0,0 +1,8 @@
+from tensorflow import TensorShape
+from tensorflow.keras.models import load_model, Sequential
+
+
+def load_friends_model(path: str = 'resources/friends_model.h5') -> Sequential:
+    model = load_model(path, compile=False)
+    model.build(TensorShape([1, None]))
+    return model
